345317341d5805cda0d3652b5d94f27f2a4e2b9b
Данил <romanovskii.ds@students.dvfu.ru>
2024-04-17 02:52:33 +1000

    some issue

--- phone_checker/main_app/tasks.py
+++ phone_checker/main_app/tasks.py
@@ -1,10 +1,16 @@
 from phone_checker.celery import app
-from .models import Person 
-import requests
-import json
-import time
+import urllib.request
 
 @app.task
 def repeat_order_make():
-    # Стягивание данных 
-    pass
+    file_1  = "https://opendata.digital.gov.ru/downloads/ABC-3xx.csv?1710948258581"
+    file_2  = "https://opendata.digital.gov.ru/downloads/ABC-3xx.csv?1710948258581"
+    file_3  = "https://opendata.digital.gov.ru/downloads/ABC-3xx.csv?1710948258581"
+    file_4  = "https://opendata.digital.gov.ru/downloads/ABC-3xx.csv?1710948258581"
+    
+    destination = '../files/'
+    
+    urllib.request.urlretrieve(file_1, destination)
+    urllib.request.urlretrieve(file_2, destination)
+    urllib.request.urlretrieve(file_3, destination)
+    urllib.request.urlretrieve(file_4, destination)
